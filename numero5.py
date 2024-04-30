75b3edf10a7dbff9e7651baed80eac9717c2846a
isaque05 <166718119+isaque05@users.noreply.github.com>
2024-04-30 09:00:18 -0300

    Update numero5.py

--- numero5.py
+++ numero5.py
@@ -1,5 +1,4 @@
 capital = float(input("Digite seu capital incial "))
 taxa = float(input("Digite sua taxa "))
-tempo = float(input("Digite seu tempo de aplicação "))
-juros = (capital * taxa * tempo)/100
-print("Seu juros é de ",juros,".")
+periodo = float(input("Digite seu periodo de aplicaçao "))
+juros = (capital * taxa * periodo)/100
